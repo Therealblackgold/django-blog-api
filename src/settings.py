cdce141777a960fa6e85f895ebb71d79491d991a
Therealblackgold <gautancholo@gmail.coml>
2022-12-20 17:42:40 +0200

    static files

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
